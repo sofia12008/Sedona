48e7a6faa70c612bcaeb8d3f479d0a7c7311558b
sofia12008 <sofianesterenko3@gmail.com>
2024-01-22 20:54:59 +0100

    Инициализация проекта + Файлы проекта

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, 'build'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
+  }
+};
